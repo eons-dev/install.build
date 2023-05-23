cfdb2f459a483cff9c825b87738acbd0cb8b2f0e
JEMV <ellismv11@gmail.com>
2023-05-22 17:50:36 -0700

    Merging install and remove merx into builder (#1)
    * Move install.merx and remove.merx into install.build with a rollback
    
    * Merge install.merx and remove.merx into this builder

--- inc/install.py
+++ inc/install.py
@@ -1,77 +1,129 @@
 import os
 import logging
 import shutil
-from distutils.file_util import copy_file
-from distutils.dir_util import copy_tree, mkpath
+import eons
+from pathlib import Path
 from ebbs import Builder
-from ebbs import OtherBuildError
 
 class install(Builder):
-    def __init__(self, name="Install a package"):
-        super().__init__(name)
+	def __init__(this, name="Install"):
+		super().__init__(name)
 
-        #Support all project types here, since we don't use the local system
-        #This bypasses type checking.
-        self.supportedProjectTypes = []
+		this.functionSucceeded = True
+		this.rollbackSucceeded = True
+		this.enableRollback = True
 
-        self.requiredKWArgs.append("repo")
+		this.requiredKWArgs.append("paths")
 
-        self.installSrcPath = "./"
-        self.installIncPath = "./"
-        self.installBinPath = "./"
+		this.optionalKWArgs["project_path"] = None
+		this.optionalKWArgs["installed_at"] = None
 
-    #Optional Builder method. See that class for details.
-    def PreBuild(self, **kwargs):
-        self.repo = kwargs.get("repo")
-        if (not len(self.repo)):
-            raise OtherBuildError('Repo credentials required to know where saved packages end up!')
+		this.result = eons.util.DotDict()
 
-        if (self.os in ['Linux']):
-            self.installSrcPath = "/usr/local/lib"
-            self.installIncPath = "/usr/local/include/"
-            self.installBinPath = "/usr/local/bin"
-        #TODO: support windows & mac
 
-        #rewrite builder code for our own uses.
-        self.package = '_'.join([self.buildPath, self.architecture])
-        details = self.buildPath.split("_")
-        self.projectType = details[0]
-        if (len(details) > 1):
-            self.projectName = '_'.join(details[1:])
+	# Reset result before each call.
+	def Initialize(this):
+		this.result = eons.util.DotDict()
+		super().Initialize()
 
-    #Required Builder method. See that class for details.
-    def Build(self):
-        packagePath = self.repo['store']
-        if (os.path.exists(packagePath)):
-            logging.info(f'DELETING {packagePath}')
-            shutil.rmtree(packagePath)
-        mkpath(packagePath)
 
-        self.executor.DownloadPackage(self.package, registerClasses=False)
-        if (not os.path.isdir(packagePath)):
-            raise OtherBuildError('Couldn\'t find downloaded package')
-        os.chdir(packagePath)
-        zipFile = f'{self.package}.zip'
-        logging.debug(f'Removing {zipFile}')
-        if (os.path.isfile(zipFile)):
-            os.remove(zipFile)
+	# Required Merx method. See that class for details.
+	def Build(this):
+		if (not this.project_path):
+			this.functionSucceeded = False
+			return this.result
 
-        if (self.projectType in ['lib']):
-            for thing in os.listdir('.'):
-                thingPath = os.path.abspath(thing)
-                if os.path.isfile(thingPath):
-                    logging.debug(f'Copying {thingPath} to {self.installSrcPath}')
-                    copy_file(thingPath, self.installSrcPath)
-                elif os.path.isdir(thingPath):
-                    destPath = os.path.join(self.installIncPath, thing)
-                    logging.debug(f'Copying {thingPath} to {destPath}')
-                    mkpath(destPath)
-                    copy_tree(thingPath, destPath)
+		logging.info(f"Installing {this.projectName}...")
+		
+		installedObjects = []
 
-        if (self.projectType in ['bin']):
-            for thing in os.listdir('.'):
-                thingPath = os.path.abspath(thing)
-                if os.path.isfile(thingPath):
-                    logging.debug(f'Copying {thingPath} to {self.installBinPath}')
-                    copy_file(thingPath, self.installBinPath)
-                #ignore folders.
+		# Assume this.paths are all valid.
+		# The dictionary this.paths comes from EMI
+		for target, destination in this.paths.items():
+			
+			# Going to each potential target
+			candidate = this.project_path.joinpath(target)
+
+			# Check whether target exists as part of Epitome
+			if (not candidate.exists()):
+				continue
+			
+			logging.debug(f"Copying files from {candidate} to {destination}")
+			#Go down every path in target and look for things
+			for thing in candidate.iterdir():
+
+				# Create variable of path of thing, but within destination path structure
+				expectedResult = Path(destination).joinpath(thing.relative_to(candidate)).resolve()
+
+				# Track paths of things expected to exist within destination path structure 
+				installedObjects.append(str(expectedResult))
+
+				# Redefine thing to its absolute path
+				thing = thing.resolve()
+				logging.debug(f"Copying {str(thing)}.")
+
+				#Depending on whether thing is a directory or file, use appropriate shutil function to copy the Epitome targets to appropriate place within destination file structure
+				if (thing.is_dir()):
+					try:
+						shutil.copytree(str(thing), expectedResult)
+					except shutil.Error as exc:
+						errors = exc.args[0]
+						for error in errors:
+							src, dst, msg = error
+							logging.debug(f"{msg}")
+				else: #thing is file
+					try:
+						shutil.copy(str(thing), expectedResult)
+					except shutil.Error as exc:
+						errors = exc.args[0]
+						for error in errors:
+							src, dst, msg = error
+							logging.debug(f"{msg}")
+
+				# Check whether the copy was successful
+				if (not expectedResult.exists()):
+					logging.error(f"COULD NOT FIND {str(expectedResult)}! Will rollback.")
+					this.functionSucceeded = False
+				logging.debug(f"Created {str(expectedResult)}.")
+
+				# Add approriate permissions to the bin and exe targets
+				if (target in ["bin", "exe"]):
+					logging.debug(f"Adding execute permissions to {str(expectedResult)}.")
+					expectedResult.chmod(0o755)
+
+
+		if (this.functionSucceeded):
+			this.result.installed_at = ";".join(installedObjects)
+			if (not os.geteuid()): #root = uid 0
+				logging.debug(f"Updating library paths.")
+				this.RunCommand(f"ldconfig {Path(this.paths['lib']).resolve()}")
+				
+		return this.result
+			
+
+	# Required Merx method. See that class for details.
+	def Rollback(this):
+		logging.info(f"Removing {this.projectName}...")
+		if (this.installed_at is None or this.installed_at is None or not len(this.installed_at)):
+			logging.debug(f"Nothing to remove for {this.projectName}")
+			return this.result
+		
+		toRemove = this.installed_at.split(';')
+		for thing in toRemove:
+			logging.debug(f"REMOVING: {thing}")
+			thing = Path(thing)
+			if (not thing.exists()):
+				logging.debug(f"Could not find {str(thing)}")
+				#That's okay. that might be why we're rolling back ;)
+				continue
+			if (thing.is_dir()):
+				shutil.rmtree(thing)
+			else:
+				thing.unlink()
+			logging.debug(f"Removed {str(thing)}")
+			
+			#TODO: error checking
+	
+		this.result.installed_at = ""
+		super().Rollback()
+		return this.result
